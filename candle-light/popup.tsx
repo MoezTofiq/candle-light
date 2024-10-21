eadfb0502790311e567d50f20756ad44e1a437fb
moeez tofiq <moeeztofiq@gmail.com>
2024-10-21 05:52:02 +0500

    switched forom mui/joy to mui/material

--- candle-light/popup.tsx
+++ candle-light/popup.tsx
@@ -1,6 +1,36 @@
+import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew"
+import Box from "@mui/material/Box"
+import IconButton from "@mui/material/IconButton"
+import Stack from "@mui/material/Stack"
+
 function IndexPopup() {
   return (
-    <div style={{ width: "100px", height: "100px" }}>need to add shit here</div>
+    <Stack
+      spacing={1}
+      direction="column"
+      useFlexGap
+      sx={{ flexWrap: "wrap", width: "750px" }}>
+      <Stack direction="row" spacing={3}>
+        <Box>donate button</Box>
+        <Box> logo button</Box>
+        <Box>
+          <IconButton>
+            <PowerSettingsNewIcon />
+          </IconButton>
+        </Box>
+      </Stack>
+
+      <Stack direction="row" spacing={3}>
+        <Box> tint color choose</Box>
+        <Box> tint opacity</Box>
+      </Stack>
+
+      <Stack direction="row" spacing={3}>
+        <Box>about me</Box>
+        <Box>other projects</Box>
+        <Box>report a bug</Box>
+      </Stack>
+    </Stack>
   )
 }
 
