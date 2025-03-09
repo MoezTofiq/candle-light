b20565cef88f8a105c1163dd909b229772b416de
Moeez Tofiq <moeeztofiq@gmail.com>
2025-03-10 00:01:10 +0500

    styling the ui

--- candle-light/Components/TimerSchedule.tsx
+++ candle-light/Components/TimerSchedule.tsx
@@ -1,28 +1,17 @@
-import GradientIcon from "@mui/icons-material/Gradient"
-import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew"
-import {
-  Checkbox,
-  CssBaseline,
-  Divider,
-  FormControlLabel,
-  Input,
-  Slider
-} from "@mui/material"
-import Box from "@mui/material/Box"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
+import { Checkbox, FormControlLabel, Slider, Switch } from "@mui/material"
+import Accordion from "@mui/material/Accordion"
+import AccordionActions from "@mui/material/AccordionActions"
+import AccordionDetails from "@mui/material/AccordionDetails"
+import AccordionSummary from "@mui/material/AccordionSummary"
 import Button from "@mui/material/Button"
-import Card from "@mui/material/Card"
-import CardContent from "@mui/material/CardContent"
-import IconButton from "@mui/material/IconButton"
 import Stack from "@mui/material/Stack"
-import { createTheme, ThemeProvider } from "@mui/material/styles"
 import Typography from "@mui/material/Typography"
-import { MuiColorInput } from "mui-color-input"
 import React, { useEffect, useState } from "react"
 
 import { sendToContentScript } from "@plasmohq/messaging"
 import { Storage } from "@plasmohq/storage"
 
-import { COLOR, OPACITY } from "~shared/defaults"
 import { log } from "~shared/helper"
 
 const TimerSchedule = () => {
@@ -118,45 +107,67 @@ const TimerSchedule = () => {
   }
 
   return (
-    <>
-      <Stack direction="row" alignItems="center" spacing={1}>
-        <Typography>Schedule Filter Activation</Typography>
-        <FormControlLabel
-          control={
-            <Checkbox checked={tempTimerEnabled} onChange={handleTimerToggle} />
-          }
-          label="Enable Timer"
-        />
-      </Stack>
-
-      {tempTimerEnabled && (
-        <>
-          <Typography>Day Time For Filter (24 Hours)</Typography>
-          <Slider
-            value={tempTimeRange}
-            onChange={handleTimeRangeChange}
-            valueLabelDisplay="auto"
-            min={0}
-            max={24}
-            step={1}
-          />
-          <Typography>Days Schedule Is Active</Typography>
-          <Stack direction="row" spacing={1} flexWrap={"wrap"}>
-            {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map(
-              (day, index) => (
-                <FormControlLabel
-                  key={index}
-                  control={
-                    <Checkbox
-                      checked={tempActiveDays[index]}
-                      onChange={() => handleDayToggle(index)}
-                    />
-                  }
-                  label={day}
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header">
+          <Typography component="span">Schedule Filter Activation</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={1}
+            mb={2}>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={tempTimerEnabled}
+                  onChange={handleTimerToggle}
                 />
-              )
-            )}
+              }
+              label="Enable Scheduler"
+            />
           </Stack>
+
+          {tempTimerEnabled && (
+            <>
+              <Typography>Day Time For Filter (24 Hours)</Typography>
+              <Slider
+                value={tempTimeRange}
+                onChange={handleTimeRangeChange}
+                valueLabelDisplay="auto"
+                min={0}
+                max={24}
+                step={1}
+              />
+              <Typography>Days Schedule Is Active</Typography>
+              <Stack
+                direction="row"
+                spacing={1}
+                justifyContent={"center"}
+                alignItems={"center"}
+                flexWrap={"wrap"}>
+                {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map(
+                  (day, index) => (
+                    <FormControlLabel
+                      key={index}
+                      control={
+                        <Checkbox
+                          checked={tempActiveDays[index]}
+                          onChange={() => handleDayToggle(index)}
+                        />
+                      }
+                      label={day}
+                    />
+                  )
+                )}
+              </Stack>
+            </>
+          )}
           <Stack
             display={"flex"}
             justifyContent={"center"}
@@ -172,9 +183,9 @@ const TimerSchedule = () => {
               Cancel
             </Button>
           </Stack>
-        </>
-      )}
-    </>
+        </AccordionDetails>
+      </Accordion>
+    </div>
   )
 }
 
