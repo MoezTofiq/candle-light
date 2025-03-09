17a5841f3fc2c25c7514be5bc1a74cba7ba394e0
Moeez Tofiq <moeeztofiq@gmail.com>
2025-03-10 04:42:01 +0500

    added messaging for the schedule timer

--- candle-light/background/messages/scheduleTimer.ts
+++ candle-light/background/messages/scheduleTimer.ts
@@ -1,9 +1,20 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
+import { Storage } from "@plasmohq/storage"
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
   const message = "temp return"
 
   console.log("got message from the pop up script ", req)
+
+  const activeDays = req.body.activeDays
+  const timeRange = req.body.timeRange
+  const timerEnabled = req.body.timerEnabled
+
+  const storage = new Storage()
+
+  await storage.set("timerEnabled", timerEnabled.toString())
+  await storage.set("timeRange", JSON.stringify(timeRange))
+  await storage.set("activeDays", JSON.stringify(activeDays))
 
   res.send({
     message
